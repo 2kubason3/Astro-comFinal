3d0fe3b2d65522bfd5930657f0f4e5c905b6a788
2kubason3 <69152413+2kubason3@users.noreply.github.com>
2022-04-28 14:06:13 +0200

    asdasd
    asdasd

---
+++ js/ajax/dodawanie.php
@@ -0,0 +1,17 @@
+<form action="dodaj.php" method="post">
+<select  name="kategoria" size="1"style="width:310px;">
+  <option value="monitory">monitory</option>
+  <option value="telewizory">telewizory</option>
+  <option value="smartfony">smartfony</option>
+  <option value="konsole">konsole</option>
+  <option value="akcesoria">akcesoria</option>
+  <option value="polecane">polecane</option>
+</select>
+<br>
+<input type="name" name="nazwa"placeholder="nazwa" style="width:300px;"><br>
+<input type="name" name="cena"placeholder="cena"style="width:300px;"><br>
+<input type="name" name="dane" placeholder="danetechniczne"style="width:300px;"><br>
+<input type="name" name="ilosc"placeholder="ilosc"style="width:300px;"><br>
+<input type="name"name="obrazek" placeholder="obrazek"style="width:300px;"><br>
+    <input type="submit" value="Dodaj Produkt">
+</form>
---
+++ js/ajax/polecane.php
@@ -0,0 +1,32 @@
+
+<?php 
+    session_start();
+    $host = "localhost";
+    $db_user = "root";
+    $db_password = "";
+   $db_name ="sklep";
+    
+    $connect = @new mysqli($host, $db_user, $db_password, $db_name);
+    if($connect->connect_errno!=0){
+        echo "Coś nie bangla".$connect->connect_errno."opis:".$connect->connect_error;
+    }
+    else{
+        $sql = "SELECT * FROM produkty WHERE kategoria='polecane'";
+        $result=mysqli_query($connect,$sql);
+        while($row=mysqli_fetch_assoc($result)){
+            echo "<a onclick='item()'>
+            <div class='item'>
+                <img alt='brak obrazka'src='".$row['obrazek']."'>
+                <span>".$row['nazwa']."</span>
+                <span class='kasa'>".$row['cena']."Zł</span>
+                <span class='kasa'>Ilość: ".$row['ilosc']."</span>
+
+                <div class='buy'>Do koszyka</div>
+            </div>
+        </a>";
+        
+        }
+       
+        $connect->close();
+    }
+?>
